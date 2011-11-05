84407737a23ba91abca1799d9f0789acde418e03
Chakravarthi Darimireddi <darimireddy@googlemail.com>
2011-11-05 12:29:32 +0530

    formatted the code.

--- apache-camel-demo/src/main/java/com/mycompany/demoapps/apachecameldemo/App.java
+++ apache-camel-demo/src/main/java/com/mycompany/demoapps/apachecameldemo/App.java
@@ -4,10 +4,9 @@ package com.mycompany.demoapps.apachecameldemo;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("Apache Camel Demo!");
+  }
 }
