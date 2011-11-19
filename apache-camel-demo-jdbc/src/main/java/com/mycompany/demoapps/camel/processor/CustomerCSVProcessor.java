371c25e294434a66d20de62280f4b797367b3e03
Chakravarthi Darimireddi <darimireddy@googlemail.com>
2011-11-19 15:18:14 +0530

    added Camle Processor for csv formatting.

---
+++ apache-camel-demo-jdbc/src/main/java/com/mycompany/demoapps/camel/processor/CustomerCSVProcessor.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.demoapps.camel.processor;
+
+import java.util.StringTokenizer;
+import java.util.logging.Logger;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+/**
+ *
+ * @author dchakr
+ */
+public class CustomerCSVProcessor implements Processor {
+
+  private static final Logger logger = Logger.getLogger(CustomerCSVProcessor.class.getName());
+
+  @SuppressWarnings("LoggerStringConcat")
+  public void process(Exchange exchng) throws Exception {
+    logger.info("::: CustomerCSVProcessor :::");
+    logger.info("::: exchng ::: " + exchng);
+
+    logger.info("::: getExchangeId ::: " + exchng.getExchangeId());
+    logger.info("::: getFromRouteId ::: " + exchng.getFromRouteId());
+    logger.info("::: getMessageId ::: " + exchng.getIn().getMessageId());
+    logger.info("::: getHeaders ::: " + exchng.getIn().getHeaders());
+
+    StringBuilder customerDataBuilder = new StringBuilder();
+
+    String body = exchng.getIn().getBody(String.class);
+    StringTokenizer columnWithValues = new StringTokenizer(body, ",");
+
+    while (columnWithValues.hasMoreTokens()) {
+      String[] valuesOnly = columnWithValues.nextToken().split("=");
+      //customerDataBuilder.append(",");
+      customerDataBuilder.append(valuesOnly[1]).append(",");
+    }
+
+    logger.info("::: customerDataBuilder ::: " + customerDataBuilder);
+    customerDataBuilder.append("\n");
+    exchng.getIn().setBody(customerDataBuilder.toString());
+  }
+}
