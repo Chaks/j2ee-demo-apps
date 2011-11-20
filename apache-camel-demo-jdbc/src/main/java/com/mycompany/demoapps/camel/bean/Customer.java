36d81e63a3dbdbc11e3ceba8937b8c5106ee9664
Chakravarthi Darimireddi <darimireddy@googlemail.com>
2011-11-20 16:24:52 +0530

    added HTTP service.

---
+++ apache-camel-demo-jdbc/src/main/java/com/mycompany/demoapps/camel/bean/Customer.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2011 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mycompany.demoapps.camel.bean;
+
+/**
+ *
+ * @author dchakr
+ */
+public class Customer {
+
+  private String customerName;
+  private String emailId;
+  private String phone;
+
+  public String getCustomerName() {
+    return customerName;
+  }
+
+  public void setCustomerName(String customerName) {
+    this.customerName = customerName;
+  }
+
+  public String getEmailId() {
+    return emailId;
+  }
+
+  public void setEmailId(String emailId) {
+    this.emailId = emailId;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+}
