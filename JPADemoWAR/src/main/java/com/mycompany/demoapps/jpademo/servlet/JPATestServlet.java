2b69bc117d996580a9ff96f90237cf7a109a56c2
Chakravarthi Darimireddi <darimireddy@googlemail.com>
2013-01-10 22:21:00 +0530

    Updated

--- JPADemoWAR/src/main/java/com/mycompany/demoapps/jpademo/servlet/JPATestServlet.java
+++ JPADemoWAR/src/main/java/com/mycompany/demoapps/jpademo/servlet/JPATestServlet.java
@@ -4,11 +4,13 @@
  */
 package com.mycompany.demoapps.jpademo.servlet;
 
+import com.mycompany.demoapps.jpademo.entity.City;
+import com.mycompany.demoapps.jpademo.entity.controller.CityJpaController;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import javax.annotation.Resource;
 import javax.persistence.*;
 import javax.servlet.ServletException;
@@ -16,20 +18,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.transaction.UserTransaction;
-
 import org.springframework.util.StopWatch;
-
-import com.mycompany.demoapps.jpademo.entity.City;
-import com.mycompany.demoapps.jpademo.entity.controller.CityJpaController;
 
 /**
  *
  * @author dchakr
  */
 public class JPATestServlet extends HttpServlet {
-  
+
   private static final Logger LOG = Logger.getLogger(JPATestServlet.class.getName());
-  @PersistenceContext  
+  @PersistenceContext
   EntityManager em;
   @Resource
   UserTransaction utx;
@@ -47,28 +45,11 @@ public class JPATestServlet extends HttpServlet {
   protected void processRequest(HttpServletRequest request, HttpServletResponse response)
           throws ServletException, IOException {
     LOG.info("Inside JPATestServlet.");
-    
-    LOG.info("::: EntityManager :::" + em.toString());
-    LOG.info("::: UserTransaction :::" + utx.toString());
-    LOG.info("::: EntityManager getClass() :::" + em.getClass());
-    
-    StopWatch stopWatch = new StopWatch("CityJpaController Stop Watch");
-    stopWatch.start("Task findCityEntities()...");
-    CityJpaController cityJpaController = new CityJpaController(utx, em.getEntityManagerFactory());
-    List<City> cityList = cityJpaController.findCityEntities();
-    
-    for (City city : cityList) {
-      LOG.info(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
-      LOG.info("::: City getName() :::" + city.getName());
-      LOG.info("::: City getDistrict() :::" + city.getDistrict());
-      LOG.info("::: City getCountryCode() :::" + city.getCountryCode());
-      LOG.info("::: City getCountryCode() :::" + city.getPopulation());
-      LOG.info(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
-    }
-    
-    stopWatch.stop();
-    LOG.info(stopWatch.prettyPrint());
-    
+
+    LOG.log(Level.INFO, "::: EntityManager :::{0}", em.toString());
+    LOG.log(Level.INFO, "::: UserTransaction :::{0}", utx.toString());
+    LOG.log(Level.INFO, "::: EntityManager getClass() :::{0}", em.getClass());
+
     response.setContentType("text/html;charset=UTF-8");
     PrintWriter out = response.getWriter();
     try {
@@ -78,6 +59,24 @@ public class JPATestServlet extends HttpServlet {
       out.println("</head>");
       out.println("<body>");
       out.println("<h1>Servlet JPATestServlet at " + request.getContextPath() + "</h1>");
+      
+      StopWatch stopWatch = new StopWatch("CityJpaController Stop Watch");
+      stopWatch.start("Task findCityEntities()...");
+      CityJpaController cityJpaController = new CityJpaController(utx, em.getEntityManagerFactory());
+      List<City> cityList = cityJpaController.findCityEntities();
+
+      for (City city : cityList) {
+        LOG.info(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+        LOG.log(Level.INFO, "::: City getName() :::{0}", city.getName());
+        LOG.log(Level.INFO, "::: City getDistrict() :::{0}", city.getDistrict());
+        LOG.log(Level.INFO, "::: City getCountryCode() :::{0}", city.getCountryCode());
+        LOG.log(Level.INFO, "::: City getCountryCode() :::{0}", city.getPopulation());
+        LOG.info(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+      }
+
+      stopWatch.stop();
+      LOG.info(stopWatch.prettyPrint());
+      
       out.println("</body>");
       out.println("</html>");
     } finally {
